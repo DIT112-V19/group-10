67cd646b16a758a6f9eeced4c166d44b8d131d2f
Katalin Ferenc <35810660+ferenckata@users.noreply.github.com>
2019-04-27 17:54:38 +0200

    add proper InstrumentedTest
    with a trial test case

--- carMobileApp/app/src/androidTest/java/com/group10app/AutonomousModeInstrumentedTest.java
+++ carMobileApp/app/src/androidTest/java/com/group10app/AutonomousModeInstrumentedTest.java
@@ -1,7 +1,10 @@
 package com.group10app;
 
+import android.content.Context;
+import android.support.test.InstrumentationRegistry;
 import android.support.test.runner.AndroidJUnit4;
 
+import org.junit.BeforeClass;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
@@ -10,17 +13,16 @@ import static org.junit.Assert.*;
 @RunWith(AndroidJUnit4.class)
 public class AutonomousModeInstrumentedTest{
 
-    private MainActivity mActivity;
-    private AutonomousMode testMode;
-
-
-    protected void setUp() throws Exception{
-        testMode = new AutonomousMode();
+    @BeforeClass
+    public static void SetUpClass(){
+        // Context of the app under test.
+        Context appContext = InstrumentationRegistry.getTargetContext();
     }
 
     @Test
-    public void DisplayedTextTest(){
+    public void CheckIfObstacleDetectedIsFalseAtStart(){
 
+        assertEquals(false,AutonomousMode.getObstacleDetected());
     }
 
 }
