2572394f537e689067811d2b07e85c7019eb760e
Katalin Ferenc <35810660+ferenckata@users.noreply.github.com>
2019-05-06 09:14:58 +0200

    tryout

--- carMobileApp/app/src/main/java/com/group10app/AutonomousMode.java
+++ carMobileApp/app/src/main/java/com/group10app/AutonomousMode.java
@@ -16,6 +16,7 @@ public class AutonomousMode extends AppCompatActivity {
     private static final String OBS = "obstacle detected";
     private static final String FINE = "driving";
     private static final String STOPPED = "stopped";
+    private static String test = ".";
 
     private UpdateStatus update = new UpdateStatus();
 
