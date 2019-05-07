4b03386cd960dff10420bb11822f0631044d5647
Gagandeep Singh <gagan0723@users.noreply.github.com>
2019-05-07 03:29:38 +0200

    Add Bluetooth functionality.
    The app connects with any device available. There are some issues with the
    ActionBar not showing.
    
    Co-authored-by: Amirneutron <43997168+Amirneutron@users.noreply.github.com>
    Co-authored-by: Gagan <gagan0723@users.noreply.github.com>

--- carMobileApp/app/src/main/java/com/group10app/ManualMode.java
+++ carMobileApp/app/src/main/java/com/group10app/ManualMode.java
@@ -18,89 +18,87 @@ import java.nio.charset.Charset;
 
 public class ManualMode extends AppCompatActivity {
 
-    ImageView forwardArrow;
-    ImageView backwardArrow;
-    ImageView rightArrow;
-    ImageView leftArrow;
-    //final BluetoothGattCharacteristic BGC =null ;
-    //final BluetoothGatt gatt = null;
-    final String LOG_TAG = ManualMode.class.getSimpleName();
+  ImageView forwardArrow;
+  ImageView backwardArrow;
+  ImageView rightArrow;
+  ImageView leftArrow;
+  //final BluetoothGattCharacteristic BGC =null ;
+  //final BluetoothGatt gatt = null;
+  final String LOG_TAG = ManualMode.class.getSimpleName();
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_manual_mode);
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_manual_mode);
 
-        forwardArrow = (ImageView) findViewById(R.id.forwardArrow);
-        backwardArrow = (ImageView) findViewById(R.id.backwardArrow);
-        rightArrow = (ImageView) findViewById(R.id.rightArrow);
-        leftArrow = (ImageView) findViewById(R.id.leftArrow);
+    forwardArrow = findViewById(R.id.forwardArrow);
+    backwardArrow = findViewById(R.id.backwardArrow);
+    rightArrow = findViewById(R.id.rightArrow);
+    leftArrow = findViewById(R.id.leftArrow);
 
-        forwardArrow.setOnClickListener(new OnClickListener() {
-            //@RequiresApi(api = Build.VERSION_CODES.JELLY_BEAN_MR2)
-            @Override
-            public void onClick(View v) {
-                String outputMessage = "/forward/";
-                //BGC_value(outputMessage);
-                connectionData("Sent: " + outputMessage);
+    forwardArrow.setOnClickListener(new OnClickListener() {
+      //@RequiresApi(api = Build.VERSION_CODES.JELLY_BEAN_MR2)
+      @Override
+      public void onClick(View v) {
+        String outputMessage = "/forward/";
+        //BGC_value(outputMessage);
+        connectionData("Sent: " + outputMessage);
+      }
+    });
 
+    backwardArrow.setOnClickListener(new OnClickListener() {
+      @RequiresApi(api = Build.VERSION_CODES.JELLY_BEAN_MR2)
+      @Override
+      public void onClick(View v) {
+        String outputMessage = "/backward/";
+        //BGC_value(outputMessage);
+        connectionData("Sent: " + outputMessage);
+      }
+    });
 
-            }
-        });
+    rightArrow.setOnClickListener(new OnClickListener() {
+      @RequiresApi(api = Build.VERSION_CODES.JELLY_BEAN_MR2)
+      @Override
+      public void onClick(View v) {
+        String outputMessage = "/right/";
+        //BGC_value(outputMessage);
+        connectionData("Sent: " + outputMessage);
+      }
+    });
 
-        backwardArrow.setOnClickListener(new OnClickListener() {
-            @RequiresApi(api = Build.VERSION_CODES.JELLY_BEAN_MR2)
-            @Override
-            public void onClick(View v) {
-                String outputMessage = "/backward/";
-                //BGC_value(outputMessage);
-                connectionData("Sent: " + outputMessage);
-            }
-        });
+    leftArrow.setOnClickListener(new OnClickListener() {
+      @RequiresApi(api = Build.VERSION_CODES.JELLY_BEAN_MR2)
+      @Override
+      public void onClick(View v) {
+        String outputMessage = "/left/";
+        //BGC_value(outputMessage);
+        connectionData("Sent: " + outputMessage);
+      }
+    });
 
-        rightArrow.setOnClickListener(new OnClickListener() {
-            @RequiresApi(api = Build.VERSION_CODES.JELLY_BEAN_MR2)
-            @Override
-            public void onClick(View v) {
-                String outputMessage = "/right/";
-                //BGC_value(outputMessage);
-                connectionData("Sent: " + outputMessage);
-            }
-        });
+    SeekBar speedControl = findViewById(R.id.seekBar2);
+    speedControl.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+      @Override
+      public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+        Log.i("Seekbar value",Integer.toString(progress));
+      }
 
-        leftArrow.setOnClickListener(new OnClickListener() {
-            @RequiresApi(api = Build.VERSION_CODES.JELLY_BEAN_MR2)
-            @Override
-            public void onClick(View v) {
-                String outputMessage = "/left/";
-                //BGC_value(outputMessage);
-                connectionData("Sent: " + outputMessage);
-            }
-        });
+      @Override
+      public void onStartTrackingTouch(SeekBar seekBar) {
 
-            SeekBar speedControl = findViewById(R.id.seekBar2);
-            speedControl.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
-            @Override
-            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
-                Log.i("Seekbar value",Integer.toString(progress));
-            }
+      }
 
-            @Override
-            public void onStartTrackingTouch(SeekBar seekBar) {
+      @Override
+      public void onStopTrackingTouch(SeekBar seekBar) {
 
-            }
+      }
+    });
+  }
 
-            @Override
-            public void onStopTrackingTouch(SeekBar seekBar) {
+  private void connectionData(final CharSequence text) {
+    Log.e(LOG_TAG, text.toString());
 
-            }
-        });
-    }
-
-    private void connectionData(final CharSequence text) {
-        Log.e(LOG_TAG, text.toString());
-
-    }
+  }
     /*private void BGC_value (String outputMessage){
         BGC.setValue(outputMessage.getBytes(Charset.forName("UTF-8")));
         if(gatt.writeCharacteristic(BGC)) {
