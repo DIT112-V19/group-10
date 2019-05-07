2becb19dcc245d286fca8ba6b4e2c538bff3a423
Katalin Ferenc <35810660+ferenckata@users.noreply.github.com>
2019-05-07 14:53:46 +0200

    fix test to run on new autonomous mode class

--- carMobileApp/app/src/androidTest/java/com/group10app/AutonomousModeInstrumentedTest.java
+++ carMobileApp/app/src/androidTest/java/com/group10app/AutonomousModeInstrumentedTest.java
@@ -1,38 +1,39 @@
 package com.group10app;
 
+import android.content.Context;
+import android.content.Intent;
 import android.support.test.InstrumentationRegistry;
 import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
 
+import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
+import static android.support.v4.content.ContextCompat.startActivity;
 import static org.junit.Assert.*;
 
 @RunWith(AndroidJUnit4.class)
 public class AutonomousModeInstrumentedTest {
 
-  private AutonomousMode autonomousMode;
-
   @Rule
-  public ActivityTestRule<AutonomousMode> mActivityRule =
-      new ActivityTestRule<>(AutonomousMode.class);
-
-
-  public AutonomousModeInstrumentedTest() {
-    this.autonomousMode = new AutonomousMode();
-  }
+  public ActivityTestRule<AutonomousMode>  mAutonomousModeTestRule=
+    new ActivityTestRule<>(AutonomousMode.class, true ,true );
 
   @BeforeClass
-  public static void SetUpClass() {
+  public static void SetUpClass(){
     // Context of the app under test.
-    InstrumentationRegistry.getTargetContext();
+    Context appContext = InstrumentationRegistry.getTargetContext();
   }
 
   @Test
-  public void CheckIfObstacleDetectedIsFalseAtStart(){
-    assertFalse(autonomousMode.getObstacleDetected());
+  public void CheckIfObstacleDetectedIsFalseWhenSet(){
+
+    mAutonomousModeTestRule.getActivity().setObstacleDetected(false);
+    assertFalse(mAutonomousModeTestRule.getActivity().getObstacleDetected());
   }
+
+
 }
